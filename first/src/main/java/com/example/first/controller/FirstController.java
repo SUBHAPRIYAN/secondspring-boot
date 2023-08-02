53326b366d2e79783eb44ff0ba592290e7204007
SUBHAPRIYAN <136422800+SUBHAPRIYAN@users.noreply.github.com>
2023-08-02 11:51:43 +0530

    Add files via upload

---
+++ first/src/main/java/com/example/first/controller/FirstController.java
@@ -0,0 +1,93 @@
+package com.example.first.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.first.model.First;
+import com.example.first.repository.FirstRepository;
+
+@RestController
+@RequestMapping("/api")
+public class FirstController {
+    @Autowired
+    FirstRepository firstRepository;
+
+    @GetMapping("/show_all")
+    
+    public List<First> getAllDetails(){
+        return (List<First>) firstRepository.findAll();
+    }
+
+    
+    @DeleteMapping("/delete_user")
+
+    public ResponseEntity<HttpStatus> deleteAllUser() {
+        firstRepository.deleteAll();
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    @PostMapping("/create_user")
+
+    public ResponseEntity<First> createuser(@RequestBody First first) {
+        First first2 = firstRepository
+                .save(new First(first.getId(), first.getFirstname(), first.getLastname(), first.getPassword()));
+
+        return new ResponseEntity<First>(first2, HttpStatus.OK);
+    }
+    
+    @PutMapping("/insert/{id}")
+
+    public ResponseEntity<First> updateUser(@PathVariable("id") Long id, @RequestBody First first) {
+
+        Optional<First> first2 = firstRepository.findById(id);
+
+        if (first2.isPresent()) {
+            First first3 = first2.get();
+            first3.setFirstname(first.getFirstname());
+            first3.setLastname(first.getLastname());
+            first3.setPassword(first.getPassword());
+            return new ResponseEntity<First>(firstRepository.save(first3), HttpStatus.OK);
+        }
+
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @GetMapping("/show_user/{id}")
+    public ResponseEntity<First> getUserById(@PathVariable("id") Long id) {
+
+        Optional<First> first2 = firstRepository.findById(id);
+
+        if (first2.isPresent()) {
+            First first3 = first2.get();
+            return new ResponseEntity<First>(first3, HttpStatus.OK);
+        }
+
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/delete_user/{id}")
+    public ResponseEntity<HttpStatus> delteUser(@PathVariable("id") Long id) {
+        firstRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+
+}
